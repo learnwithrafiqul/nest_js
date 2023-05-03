f0e256ff200a9fb50661dea9a8127c0a2f9119d3
rafiqul-altersense <rafiqul.altersense@gmail.com>
2023-05-03 18:26:06 +0600

    communication between two nestjs tested

--- src/socket/socket-client.ts
+++ src/socket/socket-client.ts
@@ -1,18 +1,31 @@
-import { Injectable, OnModuleInit } from "@nestjs/common";
-import { io, Socket } from "socket.io-client";
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { io, Socket } from 'socket.io-client';
 
 @Injectable()
 export class SocketClient implements OnModuleInit {
     public socketClient: Socket;
 
     constructor() {
-        this.socketClient = io("http://localhost:3031");
+        this.socketClient = io('http://localhost:3031');
     }
 
     onModuleInit() {
-        this.socketClient.on("connect", () => {
-            console.log("Connected to socket");
-        });
+        this.registerConsumerEvent();
     }
 
+    private registerConsumerEvent() {
+        this.socketClient.emit('newMessage', {
+            message: 'Hello World',
+        });
+        this.socketClient.on('connect', () => {
+            console.log('Connected to Gateway');
+        });
+        this.socketClient.on('onMessage', (payload: any) => {
+            console.log('payload', payload);
+        });
+
+        this.socketClient.on('disconnect', () => {
+            console.log('Disconnected from Gateway');
+        });
+    }
 }
