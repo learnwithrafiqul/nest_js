95913d4ad367676e566b7847e20e2451f75262ef
codewithrafiq <codewithrafiq@gmail.com>
2023-05-04 10:04:24 +0600

    complete

--- reactui/src/components/Websocket.tsx
+++ reactui/src/components/Websocket.tsx
@@ -1,8 +1,9 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { WebsocketContext } from "../contexts/WebsocketContext";
 
 const Websocket = () => {
   const socket = useContext(WebsocketContext);
+  const [datas, setData] = useState<any>([]);
 
   useEffect(() => {
     socket.on("connect", () => {
@@ -10,6 +11,7 @@ const Websocket = () => {
     });
     socket.on("onMessage", (data) => {
       console.log(data);
+      setData([...datas, data]);
     });
     return () => {
       console.log("Unregistered");
@@ -17,11 +19,31 @@ const Websocket = () => {
       socket.off("connect");
       socket.off("onMessage");
     };
-  }, [socket]);
+  }, [socket, datas]);
+
+  const [text, setText] = useState("");
+  const handleSubmit = (e: any) => {
+    socket.emit("newMessage", text);
+    setText("");
+  };
   return (
     <div>
       <div>
         <h1>Websocket components</h1>
+        <input
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          type="text"
+        />
+        <button onClick={handleSubmit}>Send</button>
+      </div>
+      <div className="">
+        {datas.map((data: any, index: number) => (
+          <h4 key={index}>
+            {index + 1}----{">"}
+            {JSON.stringify(data)}
+          </h4>
+        ))}
       </div>
     </div>
   );
